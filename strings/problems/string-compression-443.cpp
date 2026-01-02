31376ab6647720b3656999139d3c1d71c76d17d9
Om Pandey <omompandey7@gmail.com>
2026-01-02 09:14:42 +0530

    Add string compression solution (LeetCode 443)

---
+++ strings/problems/string-compression-443.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = chars[i];
+            int count = 0;
+
+            while (i < n && chars[i] == ch) {
+                count++;
+                i++;
+            }
+
+            chars[idx++] = ch;
+
+            if (count > 1) {
+                string str = to_string(count);
+                for (char dig : str) {
+                    chars[idx++] = dig;
+                }
+            }
+
+            i--; // adjust index
+        }
+
+        chars.resize(idx);
+        return idx;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<char> chars = {'a','a','b','b','c','c','c'};
+    int newLength = sol.compress(chars);
+
+    cout << "Compressed length: " << newLength << endl;
+    cout << "Compressed array: ";
+    for (char c : chars) {
+        cout << c << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
